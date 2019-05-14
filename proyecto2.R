48380bf97143049f4211c597ee37b0b7b5b53f58
Marcela <marce@Marcelas-MacBook-Pro-2.local>
2019-05-13 21:36:41 -0600

    clase 12

---
+++ proyecto2.R
@@ -0,0 +1,18 @@
+Proyecto 2
+
+Instrucciones para el Quiz 3: El quiz se tratará de responder a un par de preguntas de acuerdo con los resultados de un estudio de simulación simple que Ud tendrá que realizar. Para ello, hoy les estoy dando una guía general para realizar estudios de simulación, con un ejemplo llevado a cabo a través de preguntas, que aunque no tienen una respuesta, sí tienen código para resolverlas y les servirán como guía para que Uds entiendan el código y los objetivos de la simulación. En el caso del quiz, les daré una combinación de supuestos y objetivos de la simulación, y de acuerdo a una asignación aleatoria, Uds tendrán que realizarla acorde. La simulación la explicarán utilizando R markdown, y para eso pueden usar de base el archivo .Rmd que les adjunto (guía_simulaciones.Rmd).
+
+Los rubros de evaluación del quiz del jueves-viernes serán:
+  
+  1. Enviarlos en el formato correcto (usando Rmarkdown, con el nombre correcto, vía mediación y en pdf). 2pts.
+
+2. Escogencia de la combinación correcta (supuestos y objetivos) para su simulación. 1pts.
+
+3. Explicación de la simulación. 3pts
+
+4. Pregunta 1. 2pts.
+
+5. Pregunta 2.  2pts.
+
+
+
